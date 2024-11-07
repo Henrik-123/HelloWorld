d8f730cbafeb755fd2457cf6d8ed6edee22d5a41
henrikbaidori <Henrik_Baidori@hotmail.com>
2024-11-07 14:12:09 +0100

    Changes

--- src/common/HelloWorld.java
+++ src/common/HelloWorld.java
@@ -4,9 +4,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello darkness my old friend!");
 
-        System.out.println("Mer text");
 
     }
 
